cb1091192995b04c0b502d005921175f6f938862
Gábor Horváth <hgabii@inf.elte.hu>
2018-05-17 22:56:05 +0200

    Initialize project

---
+++ Tudor/src/main/java/elte/homework/TudorApp.java
@@ -0,0 +1,11 @@
+package elte.homework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TudorApp {
+    public static void main(String[] args) {
+        SpringApplication.run(TudorApp.class, args);
+    }
+}
